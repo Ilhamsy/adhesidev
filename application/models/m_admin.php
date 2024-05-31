062ebd7902eac6278585559af33c8dee9cdd13c8
Ilhamsy <ilhamsyah574@gmail.com>
2024-05-31 09:14:25 +0700

    modify models

--- application/models/m_admin.php
+++ application/models/m_admin.php
@@ -1,32 +1,29 @@
 <?php 
 
-/**
-* 
-*/
 class M_admin extends CI_model
 {
 	public function jadwal()
 {
   $data = array();
   $query = $this->db->select("*");
-  
+
   $this->db->from("jadwal");
   $this->db->join("pegawai", "pegawai.id_pegawai = jadwal.id_peg");
   $this->db->join("jabatan", "jabatan.id_jabatan = jadwal.id_jab");
   $this->db->group_by("jabatan.id_jabatan");
 
   $jadwal = $this->db->get()->result_array();
-return $jadwal; 
+return $jadwal;
 }
 
 public function get_student($id_jabatan) {
   $query = $this->db->select("*");
-  
+
   $this->db->from('jadwal');
   $this->db->join("pegawai", "pegawai.id_pegawai = jadwal.id_peg");
   $this->db->join("jabatan", "jabatan.id_jabatan = jadwal.id_jab");
   $this->db->where("jabatan.id_jabatan", $id_jabatan);
-  
+
   $jadwal_mentor = $this->db->get()->result_array();
   return $jadwal_mentor;
 }
